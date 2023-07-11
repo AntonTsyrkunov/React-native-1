751dae2fd63e1bd405b789613d7bb8de192026ef
AntonTsyrkunov <antontsyrkunov@gmail.com>
2023-07-11 11:29:01 +0200

    for inspection

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -1,10 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { useFonts } from "expo-font";
+import { RegistrationScreen } from "./Screens/RegistrationScreen";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    "Roboto-regular": require("./assets/fonts/Roboto-Regular.ttf"),
+    "Roboto-medium": require("./assets/fonts/Roboto-Medium.ttf"),
+    "Roboto-bold": require("./assets/fonts/Roboto-Bold.ttf"),
+  });
+  // if (!fontsLoaded) {
+  //   return null;
+  // }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {/* <RegistrationScreen></RegistrationScreen> */}
+      <Text>New text</Text> 
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +24,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
